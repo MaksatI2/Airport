38cb5ef42e1ff068b88aaaab47311c60d78497f5
I.Maksat <maksat@MacBook-Air-Maksat.local>
2025-05-31 11:15:31 +0600

    git init

---
+++ src/main/java/airport/AirportBookingApplication.java
@@ -0,0 +1,13 @@
+package airport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/airport/AirportBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
